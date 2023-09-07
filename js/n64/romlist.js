d5092004bf3e76128835f01469f89d9173c18b0a
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2023-09-07 08:56:55 -0400

    n64 js

---
+++ js/n64/romlist.js
@@ -0,0 +1,12 @@
+var ROMLIST = [
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/conkersbadfurday.z64",title:"Conkers Bad Fur Day"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/donkeykong64.z64",title:"Donkey Kong 64"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/majorasmask.z64",title:"The Legend of Zelda: Majoras Mask"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/mariokart64.z64",title:"Mario Kart 64"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/marioparty3.z64",title:"Mario Party 3"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/ocarinaoftime.z64",title:"The Legend of Zelda: Ocarina of Time"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/papermario.z64",title:"Paper Mario"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/pokemonstadium2.z64",title:"Pokemon Stadium 2"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/smashbros.z64",title:"Super Smash Bros"},
+    {url:"https://starlit-biscotti-dde833.netlify.app/game/n64/supermario64.z64",title:"Super Mario 64"},
+];
